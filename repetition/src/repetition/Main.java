5e3baf6442765c72a771cc46fd9b789da34b8aaa
Martin Zylauv Kristensen <zyllelol@gmail.com>
2016-04-14 12:02:57 +0200

    Opgave færdig.

--- repetition/src/repetition/Main.java
+++ repetition/src/repetition/Main.java
@@ -1,16 +1,47 @@
 package repetition;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// ?
+		Person p;
+		List<Person> personer = new ArrayList<Person>();
+		Main m = new Main(); /*
+								 * meget dumt lavet at lave denne instans af sig
+								 * selv, overhovedet ikke optimalt, men uden
+								 * denne instans af sig selv kommer der en fejl
+								 */
+
+		personer.add(p = new Person("Martin", "email@email.dk", LocalDate.of(1995, 07, 17)));
+		personer.add(p = new Person("Ole", "Ole@email.dk", LocalDate.of(1960, 05, 13)));
+		personer.add(p = new Person("Jens", "Jens@email.dk", LocalDate.of(1985, 12, 31)));
+		personer.add(p = new Person("Kim", "Kim@email.dk", LocalDate.of(2010, 12, 24)));
+		personer.add(p = new Person("Birgit", "email@email.dk", LocalDate.of(1974, 01, 01)));
+
+		System.out.println("All persons:");
+		for (Person pe : personer) {
+			System.out.println(pe.toString());
+		}
+		System.out.println();
+		System.out.println("Personer older than 25:");
+		for (Person pe : m.elderThan25(personer)) {
+			System.out.println(pe.toString());
+		}
+
 	}
-	
-	public List<Person> elderThan25(List<Person> persons) {
-		// ?
-		return null;
+
+	public List<Person> elderThan25(List<Person> personer) {
+		List<Person> elderThan25list = new ArrayList<Person>();
+		for (Person p : personer) {
+			if (p.getAge() > 25) {
+				elderThan25list.add(p);
+			}
+		}
+		return elderThan25list;
 	}
 
 }
--- repetition/src/repetition/Person.java
+++ repetition/src/repetition/Person.java
@@ -1,11 +1,15 @@
 package repetition;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
 	private String name;
 	private String email;
 	private LocalDate birthdate;
+	private LocalDate today;
+	private Period difference;
+	
 	
 	public Person() {
 	}
@@ -16,9 +20,37 @@ public class Person {
 		this.birthdate = birthdate;
 	}
 	
+	//LocalDate birthdate = new LocalDate (1970, 1, 20);
+	//LocalDate now = new LocalDate();
+	//Years age = Years.yearsBetween(birthdate, now);
+	
 	public int getAge() {
-		// ?
-		return 0;
+		today = LocalDate.now();
+		difference = Period.between(birthdate, today);
+		
+		
+		return difference.getYears();
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public void setEmail(String email){
+		this.email = email;
+	}
+	
+	public String toString(){
+		String toString = ">>Name: " + name +", Email: " + email + ", age = " + getAge()+"<<";
+		return toString;
 	}
 	
 }
