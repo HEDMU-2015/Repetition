20697ac14b812c23a2779f7e568988d81464cf3e
denierus <denieru@live.dk>
2016-04-14 11:04:23 +0200

    getters og setter + tostring implementeret

--- repetition/src/repetition/Person.java
+++ repetition/src/repetition/Person.java
@@ -16,6 +16,56 @@ public class Person {
 		this.birthdate = birthdate;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", email=" + email + ", birthdate=" + birthdate + "]";
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the birthdate
+	 */
+	public LocalDate getBirthdate() {
+		return birthdate;
+	}
+
+	/**
+	 * @param birthdate the birthdate to set
+	 */
+	public void setBirthdate(LocalDate birthdate) {
+		this.birthdate = birthdate;
+	}
+
 	public int getAge() {
 		// ?
 		return 0;
