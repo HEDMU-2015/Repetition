584fe5a213accda8874643d1bddfcc602f8998aa
Martin Zylauv Kristensen <zyllelol@gmail.com>
2016-04-14 12:03:29 +0200

    Opgave færdig v2

--- repetition/src/repetition/Person.java
+++ repetition/src/repetition/Person.java
@@ -9,48 +9,46 @@ public class Person {
 	private LocalDate birthdate;
 	private LocalDate today;
 	private Period difference;
-	
-	
+
 	public Person() {
 	}
-	
+
 	public Person(String name, String email, LocalDate birthdate) {
 		this.name = name;
 		this.email = email;
 		this.birthdate = birthdate;
 	}
-	
-	//LocalDate birthdate = new LocalDate (1970, 1, 20);
-	//LocalDate now = new LocalDate();
-	//Years age = Years.yearsBetween(birthdate, now);
-	
+
+	// LocalDate birthdate = new LocalDate (1970, 1, 20);
+	// LocalDate now = new LocalDate();
+	// Years age = Years.yearsBetween(birthdate, now);
+
 	public int getAge() {
 		today = LocalDate.now();
 		difference = Period.between(birthdate, today);
-		
-		
+
 		return difference.getYears();
 	}
-	
-	public String getName(){
+
+	public String getName() {
 		return name;
 	}
-	
-	public void setName(String name){
+
+	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public String getEmail(){
+
+	public String getEmail() {
 		return email;
 	}
-	
-	public void setEmail(String email){
+
+	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	public String toString(){
-		String toString = ">>Name: " + name +", Email: " + email + ", age = " + getAge()+"<<";
+
+	public String toString() {
+		String toString = ">>Name: " + name + ", Email: " + email + ", age = " + getAge() + "<<";
 		return toString;
 	}
-	
+
 }
