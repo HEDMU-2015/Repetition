69bd724680d08089f3dc862c6cbc93d7848af19d
Brian Laursen <bl_charlie@hotmail.com>
2016-04-19 08:11:31 +0200

    Update Person.java

--- repetition/src/repetition/Person.java
+++ repetition/src/repetition/Person.java
@@ -1,24 +1,58 @@
 package repetition;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
 	private String name;
 	private String email;
 	private LocalDate birthdate;
-	
+
 	public Person() {
 	}
-	
+
 	public Person(String name, String email, LocalDate birthdate) {
 		this.name = name;
 		this.email = email;
 		this.birthdate = birthdate;
 	}
-	
-	public int getAge() {
-		// ?
-		return 0;
+
+	public String getName() {
+		return name;
 	}
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(LocalDate birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public int getAge() {
+		LocalDate currentDate = LocalDate.now();
+		Period ageInPeriod = Period.between(birthdate, currentDate);
+		int ageInInt = ageInPeriod.getYears();
+
+		return ageInInt;
+
+	}
+
+	public String toString() {
+		return "Navn: " + name + " \t Email: - " + email + " \t birthdate: - " + birthdate;
+
+	}
+
 }
