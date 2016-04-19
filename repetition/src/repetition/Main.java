c581f41520b880e800e5bb7700419dd6b8da4106
ELISAeamv <gardinsblomst@gmail.com>
2016-04-19 08:47:09 +0200

    Min Opgave

--- repetition/src/repetition/Main.java
+++ repetition/src/repetition/Main.java
@@ -1,16 +1,48 @@
 package repetition;
+/*Færdiggør metoden "elderThan25" i Main-klassen
+- Den skal reducere input-listen, så kun personer ældre end 25 år returneres.*/
+
+import java.security.Permissions;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/* Udfyld main-metoden med følgende:
+ - Lav en List<Person> med mindst 5 Person-objekter, som er født mellem 1960 og 2010
+ - Udskriv listen
+ - Kald metoden "elderThan25"
+ - Udskriv resultatet (System.out)*/
+
 
 import java.util.List;
 
 public class Main {
+	int a = 25;
 
 	public static void main(String[] args) {
-		// ?
+		List<Person> persons = Arrays.asList(
+				new Person("Line", "email", LocalDate.of(1960, 17, 10)),
+				new Person("Erik", "email", LocalDate.of(1980, 17, 10)),
+				new Person("Thomas", "email", LocalDate.of(1990, 17, 10)),
+				new Person("Lise", "email", LocalDate.of(2000, 17, 10)),
+				new Person("Hans", "email", LocalDate.of(2010, 17, 10))
+				);
+		System.out.println(new Main().elderThan25(persons));
 	}
 	
 	public List<Person> elderThan25(List<Person> persons) {
-		// ?
-		return null;
+		List<Person> result = new ArrayList<>();
+		Iterator<Person> it = persons.iterator();
+		
+			while(it.hasNext()){
+				Person p = it.next();
+				
+				if(p.getAge() >= 25){
+					result.add(p);
+				}
+			}
+		return result;
 	}
-
+	
 }
--- repetition/src/repetition/Person.java
+++ repetition/src/repetition/Person.java
@@ -1,6 +1,11 @@
 package repetition;
-
+/* Færdiggør Person-klassen
+ - getters og setters
+ - metoden getAge() (Hint: Brug klassen java.time.Period)
+ - toString*/
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class Person {
 	private String name;
@@ -8,6 +13,7 @@ public class Person {
 	private LocalDate birthdate;
 	
 	public Person() {
+		getAge();
 	}
 	
 	public Person(String name, String email, LocalDate birthdate) {
@@ -15,10 +21,20 @@ public class Person {
 		this.email = email;
 		this.birthdate = birthdate;
 	}
+		
 	
 	public int getAge() {
-		// ?
-		return 0;
+		LocalDate today = LocalDate.now();
+		LocalDate birthday = birthdate;
+		
+	//	Period p = Period.between(birthday, today);
+		int alder = (int) ChronoUnit.YEARS.between(birthday, today);
+		
+	//	LocalDate birthdate = birthday.withYear(today.getYear());
+		System.out.println(alder + "her i getAge");
+		
+		return alder;
+	
 	}
 	
 }
