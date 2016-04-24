0489ec0809855b4b6c5729ea2084630c09e40563
Mcairborne <Mcairborne1000@gmail.com>
2016-04-24 12:25:06 +0200

    SQLs med mere

--- repetition/src/domain/Employment.java
+++ repetition/src/domain/Employment.java
@@ -6,5 +6,33 @@ public class Employment {
 	
 	int id; 
 	LocalDate employmentdate; 
-	Person person; 
+	Person person;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public LocalDate getEmploymentdate() {
+		return employmentdate;
+	}
+	public void setEmploymentdate(LocalDate employmentdate) {
+		this.employmentdate = employmentdate;
+	}
+	public Person getPerson() {
+		return person;
+	}
+	public void setPerson(Person person) {
+		this.person = person;
+	} 
+	
+	
+	
+	
+	
+	
 }
+
+
+
+
